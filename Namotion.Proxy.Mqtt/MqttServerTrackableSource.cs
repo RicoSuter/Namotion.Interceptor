ce54626a80358a581387d72fb52b59b0e544f46e
Rico Suter <mail@rsuter.com>
2024-03-24 12:39:15 +0100

    up

---
+++ Namotion.Proxy.Mqtt/MqttServerTrackableSource.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.Extensions.Hosting;
+using MQTTnet.Server;
+using MQTTnet;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Namotion.Trackable;
+using System.Reactive.Linq;
+using Namotion.Trackable.Model;
+using System.Linq;
+using System.Text.Json;
+using System.Text;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using Namotion.Trackable.Sources;
+
+namespace HomeBlaze.Mqtt
+{
+    public class MqttServerTrackableSource<TTrackable> : BackgroundService, ITrackableSource
+        where TTrackable : class
+    {
+        private readonly TrackableContext<TTrackable> _trackableContext;
+        private readonly ISourcePathProvider _sourcePathProvider;
+        private readonly ILogger _logger;
+
+        private int _numberOfClients = 0;
+        private MqttServer? _mqttServer;
+
+        private Action<PropertyInfo>? _propertyUpdateAction;
+        private ConcurrentDictionary<TrackedProperty, object?> _state = new();
+
+        public int Port { get; set; } = 1883;
+
+        public bool IsListening { get; private set; }
+
+        public int? NumberOfClients => _numberOfClients;
+
+        public MqttServerTrackableSource(
+            TrackableContext<TTrackable> trackableContext,
+            ISourcePathProvider sourcePathProvider,
+            ILogger<MqttServerTrackableSource<TTrackable>> logger)
+        {
+            _trackableContext = trackableContext;
+            _sourcePathProvider = sourcePathProvider;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _mqttServer = new MqttFactory()
+                .CreateMqttServer(new MqttServerOptions
+                {
+                    DefaultEndpointOptions =
+                    {
+                        IsEnabled = true,
+                        Port = Port
+                    }
+                });
+
+            _mqttServer.ClientConnectedAsync += ClientConnectedAsync;
+            _mqttServer.ClientDisconnectedAsync += ClientDisconnectedAsync;
+            _mqttServer.InterceptingPublishAsync += InterceptingPublishAsync;
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    await _mqttServer.StartAsync();
+                    IsListening = true;
+
+                    await Task.Delay(Timeout.Infinite, stoppingToken);
+                    await _mqttServer.StopAsync();
+
+                    IsListening = false;
+                }
+                catch (Exception ex)
+                {
+                    IsListening = false;
+
+                    _logger.LogError(ex, "Error in MQTT server.");
+                    await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+                }
+            }
+        }
+
+        public Task<IDisposable?> InitializeAsync(IEnumerable<PropertyInfo> properties, Action<PropertyInfo> propertyUpdateAction, CancellationToken cancellationToken)
+        {
+            _propertyUpdateAction = propertyUpdateAction;
+            return Task.FromResult<IDisposable?>(null);
+        }
+
+        public Task<IEnumerable<PropertyInfo>> ReadAsync(IEnumerable<PropertyInfo> properties, CancellationToken cancellationToken)
+        {
+            var propertyPaths = properties
+                .Select(p => p.Path)
+                .ToList();
+
+            return Task.FromResult<IEnumerable<PropertyInfo>>(_state
+                .Where(s => propertyPaths.Contains(_sourcePathProvider.TryGetSourcePath(s.Key)!.Replace(".", "/")))
+                .Select(s => new PropertyInfo(s.Key, null!, s.Value))
+                .ToList());
+        }
+
+        public async Task WriteAsync(IEnumerable<PropertyInfo> propertyChanges, CancellationToken cancellationToken)
+        {
+            foreach (var property in propertyChanges)
+            {
+                await _mqttServer!.InjectApplicationMessage(
+                    new InjectedMqttApplicationMessage(
+                        new MqttApplicationMessage
+                        {
+                            Topic = string.Join('/', property.Path!.Split('.')),
+                            ContentType = "application/json",
+                            PayloadSegment = new ArraySegment<byte>(
+                               Encoding.UTF8.GetBytes(JsonSerializer.Serialize(property.Value)))
+                        }));
+            }
+        }
+
+        public string? TryGetSourcePath(TrackedProperty property)
+        {
+            return _sourcePathProvider.TryGetSourcePath(property);
+        }
+
+        private Task ClientConnectedAsync(ClientConnectedEventArgs arg)
+        {
+            _numberOfClients++;
+
+            Task.Run(async () =>
+            {
+                await Task.Delay(1000);
+                foreach (var property in _trackableContext.AllProperties)
+                {
+                    await PublishPropertyValueAsync(property.GetValue(), property);
+                }
+            });
+
+            return Task.CompletedTask;
+        }
+
+        private async Task PublishPropertyValueAsync(object? value, TrackedProperty property)
+        {
+            var sourcePath = _sourcePathProvider.TryGetSourcePath(property);
+            if (sourcePath != null)
+            {
+                await _mqttServer!.InjectApplicationMessage(new InjectedMqttApplicationMessage(new MqttApplicationMessage
+                {
+                    Topic = string.Join('/', sourcePath.Split('.')),
+                    ContentType = "application/json",
+                    PayloadSegment = new ArraySegment<byte>(
+                        Encoding.UTF8.GetBytes(JsonSerializer.Serialize(value)))
+                }));
+            }
+        }
+
+        private Task InterceptingPublishAsync(InterceptingPublishEventArgs args)
+        {
+            try
+            {
+                var sourcePath = args.ApplicationMessage.Topic.Replace('/', '.');
+                var property = _trackableContext
+                    .AllProperties
+                    .SingleOrDefault(p => _sourcePathProvider.TryGetSourcePath(p) == sourcePath);
+
+                if (property != null)
+                {
+                    var payload = Encoding.UTF8.GetString(args.ApplicationMessage.PayloadSegment);
+                    var document = JsonDocument.Parse(payload);
+                    var value = document.Deserialize(property.PropertyType);
+
+                    _state[property] = value;
+                    _propertyUpdateAction?.Invoke(new PropertyInfo(property, sourcePath, value));
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to deserialize MQTT payload.");
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private Task ClientDisconnectedAsync(ClientDisconnectedEventArgs arg)
+        {
+            _numberOfClients--;
+            return Task.CompletedTask;
+        }
+    }
+}
--- Namotion.Proxy/Abstractions/ProxyPropertyChanged.cs
+++ Namotion.Proxy/Abstractions/ProxyPropertyChanged.cs
@@ -2,8 +2,11 @@
 
 public record struct ProxyPropertyChanged(
     IProxyContext Context,
+
+    // TODO: Replace with ProxyPropertyReference?
     IProxy Proxy,
     string PropertyName,
+    
     object? OldValue,
     object? NewValue)
 {
