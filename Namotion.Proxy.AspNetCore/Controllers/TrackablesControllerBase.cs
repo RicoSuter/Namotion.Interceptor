778b79eb8aef4d8968fa4e8680a800da23861eee
Rico Suter <rico.suter@buhlergroup.com>
2024-03-25 16:10:32 +0100

    up

--- Namotion.Proxy.AspNetCore/Controllers/TrackablesControllerBase.cs
+++ Namotion.Proxy.AspNetCore/Controllers/TrackablesControllerBase.cs
@@ -1,13 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text.Encodings.Web;
-using System.Text.Json;
 using System.Text.Json.Nodes;
-using System.Text.Json.Serialization;
-using System.Text.Json.Serialization.Metadata;
 
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualBasic;
+
 using Namotion.Proxy.Abstractions;
 using Namotion.Proxy.Attributes;
 
@@ -16,106 +12,25 @@ namespace Namotion.Proxy.AspNetCore.Controllers;
 public abstract class TrackablesControllerBase<TProxy> : ControllerBase
     where TProxy : class, IProxy
 {
-    private readonly static JsonSerializerOptions _options;
-
     private readonly IProxyContext _context;
     private readonly TProxy _proxy;
 
     // TODO: Inject IProxyContext<TProxy> so that multiple contexts are supported.
+
     protected TrackablesControllerBase(IProxyContext context, TProxy proxy)
     {
         _context = context;
         _proxy = proxy;
     }
 
-    static TrackablesControllerBase()
-    {
-        var options = new JsonSerializerOptions
-        {
-            PropertyNameCaseInsensitive = true,
-            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        };
-
-        void RenameAttributeProperties(JsonTypeInfo typeInfo)
-        {
-            foreach (JsonPropertyInfo propertyInfo in typeInfo.Properties)
-            {
-                var variableAttribute = propertyInfo.AttributeProvider?
-                    .GetCustomAttributes(true)
-                    .OfType<PropertyAttributeAttribute>()
-                    .FirstOrDefault();
-
-                if (variableAttribute != null)
-                {
-                    propertyInfo.Name =
-                        options.PropertyNamingPolicy.ConvertName(variableAttribute.PropertyName) + "@" +
-                        options.PropertyNamingPolicy.ConvertName(variableAttribute.AttributeName);
-                }
-            }
-        }
-
-        options.Converters.Add(new JsonStringEnumConverter());
-        options.TypeInfoResolver = new DefaultJsonTypeInfoResolver
-        {
-            Modifiers = { RenameAttributeProperties }
-        };
-
-        _options = options;
-    }
-
     [HttpGet]
     public ActionResult<TProxy> GetVariables()
     {
+        // TODO: correctly generate OpenAPI schema
+
         var jsonObject = new JsonObject();
         Populate(_proxy, _context.GetHandler<IProxyRegistry>(), jsonObject);
-
-        //var json = JsonSerializer.SerializeToElement(_proxy, _options);
         return Ok(jsonObject);
-    }
-
-    public static void Populate(IProxy proxy, IProxyRegistry register, JsonObject obj)
-    {
-        if (register.KnownProxies.TryGetValue(proxy, out var metadata))
-        {
-            foreach (var property in metadata.Properties.Where(p => p.Value.GetValue is not null))
-            {
-                var key = property.Key;
-               
-                var attribute = property.Value.Attributes
-                    .OfType<PropertyAttributeAttribute>()
-                    .FirstOrDefault();
-
-                if (attribute is not null)
-                {
-                    key = attribute.PropertyName + "@" + attribute.AttributeName;
-                }
-
-                var value = property.Value.GetValue?.Invoke();
-                if (value is IProxy childProxy)
-                {
-                    var child = new JsonObject();
-                    Populate(childProxy, register, child);
-                    obj[key] = child;
-                }
-                else if (value is IEnumerable<IProxy> childProxies)
-                {
-                    var children = new JsonArray();
-                    foreach (var childProxy2 in childProxies)
-                    {
-                        var child = new JsonObject();
-                        Populate(childProxy2, register, child);
-                        children.Add(child);
-                    }
-                    obj[key] = children;
-                }
-                else
-                {
-                    obj[key] = JsonValue.Create(value);
-                }
-            }
-        }
     }
 
     //[HttpPost]
@@ -217,4 +132,53 @@ public abstract class TrackablesControllerBase<TProxy> : ControllerBase
     //        .AllProperties
     //        .Where(p => !p.IsAttribute && allTrackers.Any(t => t.ParentProperty == p) == false));
     //}
+
+    private static void Populate(IProxy proxy, IProxyRegistry register, JsonObject obj)
+    {
+        if (register.KnownProxies.TryGetValue(proxy, out var metadata))
+        {
+            foreach (var property in metadata.Properties.Where(p => p.Value.GetValue is not null))
+            {
+                var name = GetPropertyName(metadata, property.Key, property.Value);
+                var value = property.Value.GetValue?.Invoke();
+                if (value is IProxy childProxy)
+                {
+                    var child = new JsonObject();
+                    Populate(childProxy, register, child);
+                    obj[name] = child;
+                }
+                else if (value is IEnumerable<IProxy> collection)
+                {
+                    var children = new JsonArray();
+                    foreach (var arrayProxyItem in collection)
+                    {
+                        var child = new JsonObject();
+                        Populate(arrayProxyItem, register, child);
+                        children.Add(child);
+                    }
+                    obj[name] = children;
+                }
+                else
+                {
+                    obj[name] = JsonValue.Create(value);
+                }
+            }
+        }
+    }
+
+    private static string GetPropertyName(ProxyMetadata metadata, string name, ProxyProperty property)
+    {
+        var attribute = property.Attributes
+            .OfType<PropertyAttributeAttribute>()
+            .FirstOrDefault();
+
+        if (attribute is not null)
+        {
+            return GetPropertyName(metadata,
+                attribute.PropertyName,
+                metadata.Properties[attribute.PropertyName]) + "@" + attribute.AttributeName;
+        }
+
+        return name; // TODO: apply JSON naming policy
+    }
 }
