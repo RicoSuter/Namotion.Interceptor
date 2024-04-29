6f7271823feccd653637e74451797510dd9a66f9
Rico Suter <mail@rsuter.com>
2024-04-29 10:39:57 +0200

    up

---
+++ src/Namotion.Proxy.OpcUa/Server/CustomNodeManagerFactory.cs
@@ -0,0 +1,32 @@
+﻿using Opc.Ua.Server;
+using Opc.Ua;
+
+namespace Namotion.Proxy.OpcUa.Server;
+
+internal class CustomNodeManagerFactory<TProxy> : INodeManagerFactory
+    where TProxy : IProxy
+{
+    private readonly TProxy _proxy;
+    private readonly OpcUaServerTrackableSource<TProxy> _source;
+    private readonly string? _rootName;
+
+    public StringCollection NamespacesUris => new StringCollection(new[]
+    {
+        "https://foobar/",
+        "http://opcfoundation.org/UA/",
+        "http://opcfoundation.org/UA/DI/",
+        "http://opcfoundation.org/UA/Machinery/"
+    });
+
+    public CustomNodeManagerFactory(TProxy proxy, OpcUaServerTrackableSource<TProxy> source, string? rootName)
+    {
+        _proxy = proxy;
+        _source = source;
+        _rootName = rootName;
+    }
+
+    public INodeManager Create(IServerInternal server, ApplicationConfiguration configuration)
+    {
+        return new CustomNodeManager<TProxy>(_proxy, _source, server, configuration, _rootName);
+    }
+}
