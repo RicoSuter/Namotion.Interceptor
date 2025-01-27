9cfa2ac7e302297bea37b042ba0be28e1fbf895d
Rico Suter <mail@rsuter.com>
2025-01-27 09:47:30 +0100

    update

---
+++ src/Namotion.Interceptor.Tests/HierarchicalInterceptorCollectionTests.cs
@@ -0,0 +1,17 @@
+namespace Namotion.Interceptor.Tests;
+
+public class HierarchicalInterceptorCollectionTests
+{
+    [Fact]
+    public void WhenChangingPropertyWhichIsUsedInDerivedProperty_ThenDerivedPropertyIsChanged()
+    {
+        // Arrange
+        var collection = new HierarchicalInterceptorCollection();
+
+        // Act
+
+
+        // Assert
+
+    }
+}
