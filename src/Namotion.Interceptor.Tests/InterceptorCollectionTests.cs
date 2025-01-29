953a77fbe1b738a8da5cf41143df30326d1d45f6
Rico Suter <mail@rsuter.com>
2025-01-29 21:19:05 +0100

    add test

--- src/Namotion.Interceptor.Tests/InterceptorCollectionTests.cs
+++ src/Namotion.Interceptor.Tests/InterceptorCollectionTests.cs
@@ -36,4 +36,23 @@ public class InterceptorCollectionTests
         
         Assert.Throws<InvalidOperationException>(() => collection.GetService<int>());
     }
+    
+    [Fact]
+    public void WhenCollectionHasSubCollection_ThenServicesAreInherited()
+    {
+        // Arrange
+        var collection1 = new InterceptorCollection();
+        var collection2 = new InterceptorCollection();
+        
+        collection2.AddInterceptorCollection(collection1);
+
+        // Act
+        collection1.AddService(1);
+        collection1.AddService(2);
+        collection2.AddService(3);
+
+        // Assert
+        Assert.Equal(2, collection1.GetServices<int>().Count());
+        Assert.Equal(3, collection2.GetServices<int>().Count());
+    }
 }
