be842b93310193e8498ea93ab79c3777d986d546
Rico Suter <mail@rsuter.com>
2025-02-07 22:54:59 +0100

    Add Namotion.Interceptor.Hosting

--- src/Namotion.Interceptor.SampleWeb/Program.cs
+++ src/Namotion.Interceptor.SampleWeb/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Namotion.Interceptor.AspNetCore.Controllers;
 using Namotion.Interceptor.Attributes;
+using Namotion.Interceptor.Hosting;
 using Namotion.Interceptor.Registry;
 using Namotion.Interceptor.Registry.Abstractions;
 using Namotion.Interceptor.Registry.Attributes;
@@ -40,7 +41,7 @@ namespace Namotion.Interceptor.SampleWeb
     }
 
     [InterceptorSubject]
-    public partial class Tire
+    public partial class Tire : BackgroundService
     {
         [SourceName("mqtt", "pressure")]
         [SourceName("opc", "Pressure")]
@@ -58,6 +59,16 @@ namespace Namotion.Interceptor.SampleWeb
         public Tire()
         {
             Pressure_Minimum = 0.0m;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // This is automatically started by .WithHostedServices()
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(1000, stoppingToken);
+                Console.WriteLine("Current pressure: " + Pressure);
+            }
         }
     }
 
@@ -87,7 +98,8 @@ namespace Namotion.Interceptor.SampleWeb
                 .WithRegistry()
                 .WithFullPropertyTracking()
                 .WithLifecycle()
-                .WithDataAnnotationValidation();
+                .WithDataAnnotationValidation()
+                .WithHostedServices(builder.Services);
 
             var car = new Car(context);
 
