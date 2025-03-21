502dcc9eedf32aad027f405c5001ac416cc61361
Rico Suter <mail@rsuter.com>
2025-03-21 16:34:42 +0100

    update

--- src/Namotion.Interceptor.Sources/ISubjectSource.cs
+++ src/Namotion.Interceptor.Sources/ISubjectSource.cs
@@ -8,7 +8,12 @@ public interface ISubjectSource
     
     Task<IDisposable?> InitializeAsync(ISubjectSourceManager manager, CancellationToken cancellationToken);
 
-    public Task<Action> LoadFullSourceStateAsync(CancellationToken cancellationToken) => Task.FromResult(() => { });
+    /// <summary>
+    /// Loads the complete state of the source and applies it to the subject in the returned callback.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The callback which applies the loaded state to the subject.</returns>
+    public Task<Action> LoadCompleteSourceStateAsync(CancellationToken cancellationToken) => Task.FromResult(() => { });
     
     Task WriteToSourceAsync(IEnumerable<PropertyChangedContext> updates, CancellationToken cancellationToken);
 }
