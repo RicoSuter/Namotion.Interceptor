6c43bf497344fa0bacf2f4fd6ccc217b171ded79
Rico Suter <mail@rsuter.com>
2025-03-23 01:07:14 +0100

    move

--- src/Namotion.Interceptor.Sources/SubjectDataExtensions.cs
+++ src/Namotion.Interceptor.Sources/SubjectDataExtensions.cs
@@ -1,21 +1,24 @@
-﻿using Namotion.Interceptor.Registry;
-using Namotion.Interceptor.Tracking.Change;
+﻿using Namotion.Interceptor.Tracking.Change;
 
-namespace Namotion.Interceptor.Sources.Extensions;
+namespace Namotion.Interceptor.Sources;
 
 public static class SubjectDataExtensions
 {
     private const string IsChangingFromSourceKey = "Namotion.IsChangingFromSource";
 
+    /// <summary>
+    /// Sets the value of the property and marks the assignment as applied by the specified source.
+    /// </summary>
+    /// <param name="property">The property.</param>
+    /// <param name="source">The source.</param>
+    /// <param name="valueFromSource">The value</param>
     public static void SetValueFromSource(this PropertyReference property, ISubjectSource source, object? valueFromSource)
     {
         // TODO: Use async local here instead? Verify correctness of the method
 
         var contexts = property.GetOrAddPropertyData(IsChangingFromSourceKey, () => new HashSet<ISubjectSource>())!;
         lock (contexts)
-        {
             contexts.Add(source);
-        }
 
         try
         {
@@ -24,12 +27,16 @@ public static class SubjectDataExtensions
         finally
         {
             lock (contexts)
-            {
                 contexts.Remove(source);
-            }
         }
     }
 
+    /// <summary>
+    /// Checks if the property change is from the specified source.
+    /// </summary>
+    /// <param name="change">The property change.</param>
+    /// <param name="source">The source find.</param>
+    /// <returns>The result.</returns>
     public static bool IsChangingFromSource(this SubjectPropertyChange change, ISubjectSource source)
     {
         var contexts = change.Property.GetOrAddPropertyData(IsChangingFromSourceKey, () => new HashSet<ISubjectSource>())!;
