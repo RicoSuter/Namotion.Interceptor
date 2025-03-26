5c6582d932fdd81f9e6388e235811b23b80fb778
Rico Suter <mail@rsuter.com>
2025-03-26 19:51:13 +0100

    upate

--- src/Namotion.Interceptor.Tracking/Change/PropertyChangedObservable.cs
+++ src/Namotion.Interceptor.Tracking/Change/PropertyChangedObservable.cs
@@ -13,7 +13,7 @@ public class PropertyChangedObservable : IObservable<SubjectPropertyChange>, IWr
 
         var changedContext = new SubjectPropertyChange(
             context.Property, 
-            context.Property.GetDataSnapshot(),
+            context.Property.GetSource(),
             SubjectMutationContext.GetCurrentTimestamp(),
             currentValue, result);
 
--- src/Namotion.Interceptor.Tracking/Change/SubjectPropertyChange.cs
+++ src/Namotion.Interceptor.Tracking/Change/SubjectPropertyChange.cs
@@ -2,13 +2,9 @@
 
 public record struct SubjectPropertyChange(
     PropertyReference Property,
-    IReadOnlyDictionary<string, object?> PropertyDataSnapshot,
+    object? Source,
     DateTimeOffset Timestamp,
     object? OldValue,
     object? NewValue)
 {
-    public bool TryGetPropertySnapshotData(string key, out object? value)
-    {
-        return PropertyDataSnapshot.TryGetValue($"{Property.Name}:{key}", out value)!;
-    }
 }
