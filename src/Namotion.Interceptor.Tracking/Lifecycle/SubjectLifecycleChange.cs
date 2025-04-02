3a80f52fda3243c3f6ce7263f824d1790394ba7c
Rico Suter <mail@rsuter.com>
2025-04-02 22:10:40 +0200

    up

--- src/Namotion.Interceptor.Tracking/Lifecycle/SubjectLifecycleChange.cs
+++ src/Namotion.Interceptor.Tracking/Lifecycle/SubjectLifecycleChange.cs
@@ -1,5 +1,9 @@
 ﻿namespace Namotion.Interceptor.Tracking.Lifecycle;
 
+/// <param name="Subject">Gets the subject where a property reference pointing to it has been changed.</param>
+/// <param name="Property">Gets the property which has been changed.</param>
+/// <param name="Index">Gets the index defining the place of the subject in the property's dictionary or collection.</param>
+/// <param name="ReferenceCount">Gets the number of properties pointing to the referenced subject.</param>
 public record struct SubjectLifecycleChange(
     IInterceptorSubject Subject,
     PropertyReference? Property,
