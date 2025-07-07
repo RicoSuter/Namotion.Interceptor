b8a1cd3baa6778b40b2abf7886d552c97e7b94b8
Rico Suter <mail@rsuter.com>
2025-07-07 17:57:40 +0200

    update

--- src/Namotion.Interceptor.Registry.Tests/AttributeTests.cs
+++ src/Namotion.Interceptor.Registry.Tests/AttributeTests.cs
@@ -1,5 +1,4 @@
 using System.Reactive.Linq;
-using Namotion.Interceptor.Attributes;
 using Namotion.Interceptor.Registry.Tests.Models;
 using Namotion.Interceptor.Tracking;
 using Namotion.Interceptor.Tracking.Change;
@@ -56,7 +55,7 @@ public class AttributeTests
         var newValue = attribute2.GetValue();
         Assert.Equal(500, newValue);
     }
-    
+
     [Fact]
     public void WhenRegisteringDynamicDerivedProperty_ThanChangeTrackingIsWorking()
     {
@@ -67,25 +66,130 @@ public class AttributeTests
             .WithDerivedPropertyChangeDetection()
             .WithRegistry();
 
-        // Act
         var person = new Person(context)
         {
             FirstName = "John"
         };
 
         var changes = new List<SubjectPropertyChange>();
+
+        // Act
         var dynamicDerivedProperty = person
             .TryGetRegisteredSubject()!
-            .AddProperty("DynamicDerivedProperty", typeof(string), _ => "Mr. " + person.FirstName, null, new DerivedAttribute());
+            .AddDerivedProperty("DynamicDerivedProperty", typeof(string), _ => "Mr. " + person.FirstName);
 
         context
             .GetPropertyChangedObservable()
             .Where(c => c.Property == dynamicDerivedProperty.Property)
             .Subscribe(a => changes.Add(a));
-        
+
         person.FirstName = "Rico";
 
         // Assert
         Assert.Contains(changes, x => x.NewValue!.Equals("Mr. Rico"));
+    }
+
+    [Fact]
+    public void WhenRegisteringDynamicDerivedAttribute_ThanChangeTrackingIsWorking()
+    {
+        // Arrange
+        var context = InterceptorSubjectContext
+            .Create()
+            .WithPropertyChangedObservable()
+            .WithDerivedPropertyChangeDetection()
+            .WithRegistry();
+
+        var person = new Person(context)
+        {
+            FirstName = "John"
+        };
+
+        var changes = new List<SubjectPropertyChange>();
+
+        // Act
+        var dynamicDerivedAttribute = person
+            .TryGetRegisteredSubject()!
+            .TryGetProperty(nameof(Person.FirstName))!
+            .AddDerivedAttribute("DynamicDerivedAttribute", typeof(string), _ => "Mr. " + person.FirstName);
+
+        context
+            .GetPropertyChangedObservable()
+            .Where(c => c.Property == dynamicDerivedAttribute.Property)
+            .Subscribe(a => changes.Add(a));
+
+        person.FirstName = "Rico";
+
+        // Assert
+        Assert.Contains(changes, x => x.NewValue!.Equals("Mr. Rico"));
+    }
+
+    [Fact]
+    public void WhenRegisteringDynamicProperty_ThanChangeTrackingIsWorking()
+    {
+        // Arrange
+        var context = InterceptorSubjectContext
+            .Create()
+            .WithPropertyChangedObservable()
+            .WithDerivedPropertyChangeDetection()
+            .WithRegistry();
+
+        var person = new Person(context)
+        {
+            FirstName = "John"
+        };
+
+        var value = "Test";
+
+        var changes = new List<SubjectPropertyChange>();
+
+        // Act
+        var dynamicProperty = person
+            .TryGetRegisteredSubject()!
+            .AddProperty("DynamicProperty", typeof(string), _ => value, (_, v) => value = (string)v!);
+
+        context
+            .GetPropertyChangedObservable()
+            .Where(c => c.Property == dynamicProperty.Property)
+            .Subscribe(a => changes.Add(a));
+
+        dynamicProperty.SetValue("Abc");
+
+        // Assert
+        Assert.Contains(changes, x => x.NewValue!.Equals("Abc"));
+    }
+
+    [Fact]
+    public void WhenRegisteringDynamicAttribute_ThanChangeTrackingIsWorking()
+    {
+        // Arrange
+        var context = InterceptorSubjectContext
+            .Create()
+            .WithPropertyChangedObservable()
+            .WithDerivedPropertyChangeDetection()
+            .WithRegistry();
+
+        var person = new Person(context)
+        {
+            FirstName = "John"
+        };
+
+        var value = "Test";
+        var changes = new List<SubjectPropertyChange>();
+
+        // Act
+        var dynamicAttribute = person
+            .TryGetRegisteredSubject()!
+            .TryGetProperty(nameof(Person.FirstName))!
+            .AddAttribute("DynamicAttribute", typeof(string), _ => value, (_, v) => value = (string)v!);
+
+        context
+            .GetPropertyChangedObservable()
+            .Where(c => c.Property == dynamicAttribute.Property)
+            .Subscribe(a => changes.Add(a));
+
+        dynamicAttribute.SetValue("Abc");
+
+        // Assert
+        Assert.Contains(changes, x => x.NewValue!.Equals("Abc"));
     }
 }
