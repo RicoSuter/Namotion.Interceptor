01e25376327ecf9bdfee0005778de8451e3482e2
Rico Suter <mail@rsuter.com>
2025-07-07 17:05:32 +0200

    fix: Validate only static properties in DataAnnotationsValidator

--- src/Namotion.Interceptor/SubjectPropertyMetadata.cs
+++ src/Namotion.Interceptor/SubjectPropertyMetadata.cs
@@ -11,15 +11,19 @@ public readonly record struct SubjectPropertyMetadata
     public Func<object, object?>? GetValue { get; }
     
     public Action<object, object?>? SetValue { get; }
+    
+    public bool IsDynamic { get; }
 
     public SubjectPropertyMetadata(
         string name, Type type, IReadOnlyCollection<Attribute> attributes, 
-        Func<object, object?>? getValue, Action<object, object?>? setValue)
+        Func<object, object?>? getValue, Action<object, object?>? setValue,
+        bool isDynamic)
     {
         Name = name;
         Type = type;
         Attributes = attributes;
         GetValue = getValue;
         SetValue = setValue;
+        IsDynamic = isDynamic;
     }
 }
