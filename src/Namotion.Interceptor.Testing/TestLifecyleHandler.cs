2fb2b00780aec87df4bfe00201644434043b5699
Rico Suter <mail@rsuter.com>
2025-07-14 00:45:11 +0200

    Improve lifecycle interceptor performance

--- src/Namotion.Interceptor.Testing/TestLifecyleHandler.cs
+++ src/Namotion.Interceptor.Testing/TestLifecyleHandler.cs
@@ -4,25 +4,21 @@ namespace Namotion.Interceptor.Testing
 {
     public class TestLifecyleHandler : ILifecycleHandler
     {
-        private readonly List<SubjectLifecycleChange> _attaches;
-        private readonly List<SubjectLifecycleChange> _detaches;
+        private readonly List<string> _events;
 
-        public TestLifecyleHandler(
-            List<SubjectLifecycleChange> attaches,
-            List<SubjectLifecycleChange> detaches)
+        public TestLifecyleHandler(List<string> events)
         {
-            _attaches = attaches;
-            _detaches = detaches;
+            _events = events;
         }
 
         public void AttachSubject(SubjectLifecycleChange change)
         {
-            _attaches.Add(change);
+            _events.Add($"Attached: {change.Subject} at {change.Property?.Name} with index {change.Index}, count: {change.ReferenceCount}");
         }
 
         public void DetachSubject(SubjectLifecycleChange change)
         {
-            _detaches.Add(change);
+            _events.Add($"Detached: {change.Subject} at {change.Property?.Name} with index {change.Index}, count: {change.ReferenceCount}");
         }
     }
 }
