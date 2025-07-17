c3e4e03ae3b1b6b922afab546d80d1290353f7fe
Rico Suter <mail@rsuter.com>
2025-07-18 00:08:17 +0200

    add benchmark

---
+++ src/Namotion.Interceptor.Benchmark/SubjectUpdateBenchmark.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using Namotion.Interceptor.Registry;
+using Namotion.Interceptor.Sources.Paths;
+using Namotion.Interceptor.Sources.Updates;
+using Namotion.Interceptor.Tracking;
+using Namotion.Interceptor.Tracking.Change;
+
+namespace Namotion.Interceptor.Benchmark;
+
+#pragma warning disable CS8618
+
+[MemoryDiagnoser]
+public class SubjectUpdateBenchmark
+{
+    private Car _car;
+    private SubjectPropertyChange[] _changes;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var context = InterceptorSubjectContext
+            .Create()
+            .WithRegistry();
+
+        _car = new Car(context);
+        _changes =
+        [
+            new SubjectPropertyChange(new PropertyReference(_car.Tires[2], "Pressure"), null, DateTimeOffset.Now, 10d, 42d),
+            new SubjectPropertyChange(new PropertyReference(_car, "Name"), null, DateTimeOffset.Now, "OldName", "NewName"),
+            new SubjectPropertyChange(new PropertyReference(_car.Tires[1], "Pressure"), null, DateTimeOffset.Now, 10d, 42d),
+        ];
+    }
+
+    [Benchmark]
+    public void CreateCompleteUpdate()
+    {
+        var subjectUpdate = SubjectUpdate
+            .CreateCompleteUpdate(_car)
+            .ConvertToJsonCamelCasePath();
+    }
+    
+    [Benchmark]
+    public void CreatePartialUpdate()
+    {
+        var partialSubjectUpdate = SubjectUpdate
+            .CreatePartialUpdateFromChanges(_car, _changes)
+            .ConvertToJsonCamelCasePath();    
+    }
+}
