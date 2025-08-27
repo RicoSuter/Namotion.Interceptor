ca4fb20e4ceb8f5f5192a7ea022c79018bfbd14f
Rico Suter <mail@rsuter.com>
2025-08-27 09:39:50 +0200

    up

--- src/Namotion.Interceptor/InterceptorSubjectContext.cs
+++ src/Namotion.Interceptor/InterceptorSubjectContext.cs
@@ -46,18 +46,8 @@ public class InterceptorSubjectContext : IInterceptorSubjectContext
         var func = (Func<ReadPropertyInterception, Func<IInterceptorSubject, TProperty>, TProperty>)
             _readInterceptorFunction.GetOrAdd(typeof(TProperty), _ =>
             {
-                var returnReadValue = new Func<ReadPropertyInterception, Func<IInterceptorSubject, TProperty>, TProperty>(
-                    (i, innerReadValue) => innerReadValue(i.Property.Subject));
-
                 var readInterceptors = GetServices<IReadInterceptor>();
-                foreach (var handler in readInterceptors)
-                {
-                    var previousReadValue = returnReadValue;
-                    returnReadValue = (context, innerReadValue) =>
-                        handler.ReadProperty(context, ctx => previousReadValue(ctx, innerReadValue));
-                }
-
-                return returnReadValue;
+                return ReadInterceptorChain<TProperty>.Create(readInterceptors);
             });
 
         return func(interception, readValue);
@@ -75,22 +65,8 @@ public class InterceptorSubjectContext : IInterceptorSubjectContext
         var func = (Func<WritePropertyInterception, Action<IInterceptorSubject, TProperty>, TProperty>)
             _writeInterceptorFunction.GetOrAdd(typeof(TProperty), _ =>
             {
-                var returnWriteValue = new Func<WritePropertyInterception, Action<IInterceptorSubject, TProperty>, TProperty>(
-                    (i, innerWriteValue) =>
-                    {
-                        innerWriteValue(i.Property.Subject, (TProperty)i.NewValue!);
-                        return (TProperty)i.NewValue!;
-                    });
-
-                var readInterceptors = GetServices<IWriteInterceptor>();
-                foreach (var handler in readInterceptors)
-                {
-                    var previousWriteValue = returnWriteValue;
-                    returnWriteValue = (context, innerWriteValue) =>
-                        handler.WriteProperty(context, ctx => previousWriteValue(ctx, innerWriteValue));
-                }
-
-                return returnWriteValue;
+                var writeInterceptors = GetServices<IWriteInterceptor>();
+                return WriteInterceptorChain<TProperty>.Create(writeInterceptors);
             });
 
         func(interception, writeValue);
@@ -211,6 +187,134 @@ public class InterceptorSubjectContext : IInterceptorSubjectContext
         foreach (var parent in _usedByContexts)
         {
             parent.OnContextChanged();
+        }
+    }
+
+    private static class ReadInterceptorChain<TProperty>
+    {
+        public static Func<ReadPropertyInterception, Func<IInterceptorSubject, TProperty>, TProperty> Create(IEnumerable<IReadInterceptor> interceptors)
+        {
+            var interceptorArray = interceptors.ToArray();
+            if (interceptorArray.Length == 0)
+            {
+                return (interception, innerReadValue) => innerReadValue(interception.Property.Subject);
+            }
+
+            // Create a pre-built chain that uses a stateful executor to avoid allocations
+            var chain = new AllocationFreeChain<ReadPropertyInterception, IReadInterceptor, TProperty>(
+                interceptorArray,
+                (interceptor, context, next) => interceptor.ReadProperty(context, next),
+                (interception, innerReadValue) => ((Func<IInterceptorSubject, TProperty>)innerReadValue)(interception.Property.Subject)
+            );
+            return (interception, innerReadValue) => chain.Execute(interception, innerReadValue);
+        }
+    }
+
+    private static class WriteInterceptorChain<TProperty>
+    {
+        public static Func<WritePropertyInterception, Action<IInterceptorSubject, TProperty>, TProperty> Create(IEnumerable<IWriteInterceptor> interceptors)
+        {
+            var interceptorArray = interceptors.ToArray();
+            if (interceptorArray.Length == 0)
+            {
+                return (interception, innerWriteValue) =>
+                {
+                    innerWriteValue(interception.Property.Subject, (TProperty)interception.NewValue!);
+                    return (TProperty)interception.NewValue!;
+                };
+            }
+
+            // Create a pre-built chain that uses a stateful executor to avoid allocations
+            var chain = new AllocationFreeChain<WritePropertyInterception, IWriteInterceptor, TProperty>(
+                interceptorArray,
+                (interceptor, context, next) => interceptor.WriteProperty(context, next),
+                (interception, innerWriteValue) =>
+                {
+                    var writeAction = (Action<IInterceptorSubject, TProperty>)innerWriteValue;
+                    writeAction(interception.Property.Subject, (TProperty)interception.NewValue!);
+                    return (TProperty)interception.NewValue!;
+                }
+            );
+            return (interception, innerWriteValue) => chain.Execute(interception, innerWriteValue);
+        }
+    }
+
+    private sealed class AllocationFreeChain<TContext, TInterceptor, TProperty>
+    {
+        private readonly TInterceptor[] _interceptors;
+        private readonly ChainNode[] _nodes;
+        private readonly Func<TInterceptor, TContext, Func<TContext, TProperty>, TProperty> _executeInterceptor;
+        private readonly Func<TContext, object, TProperty> _executeTerminal;
+
+        public AllocationFreeChain(
+            TInterceptor[] interceptors,
+            Func<TInterceptor, TContext, Func<TContext, TProperty>, TProperty> executeInterceptor,
+            Func<TContext, object, TProperty> executeTerminal)
+        {
+            _interceptors = interceptors;
+            _executeInterceptor = executeInterceptor;
+            _executeTerminal = executeTerminal;
+            _nodes = new ChainNode[interceptors.Length];
+            
+            // Pre-build the chain nodes with their continuation functions
+            for (int i = 0; i < interceptors.Length; i++)
+            {
+                _nodes[i] = new ChainNode(this, i);
+            }
+        }
+
+        public TProperty Execute(TContext context, object terminal)
+        {
+            // Use the pre-built chain without creating any new delegates
+            return ExecuteAtIndex(0, context, terminal);
+        }
+
+        private TProperty ExecuteAtIndex(int index, TContext context, object terminal)
+        {
+            if (index >= _interceptors.Length)
+            {
+                return _executeTerminal(context, terminal);
+            }
+
+            var interceptor = _interceptors[index];
+            var node = _nodes[index];
+            
+            // Set the current execution context in the node
+            node.SetContext(terminal);
+            
+            // Execute the interceptor with the pre-built continuation
+            return _executeInterceptor(interceptor, context, node.GetContinuation());
+        }
+
+        private sealed class ChainNode
+        {
+            private readonly AllocationFreeChain<TContext, TInterceptor, TProperty> _chain;
+            private readonly int _nextIndex;
+            private readonly Func<TContext, TProperty> _continuation;
+            
+            private object _currentTerminal = null!;
+
+            public ChainNode(AllocationFreeChain<TContext, TInterceptor, TProperty> chain, int currentIndex)
+            {
+                _chain = chain;
+                _nextIndex = currentIndex + 1;
+                _continuation = ExecuteNext; // Pre-build the continuation delegate
+            }
+
+            public void SetContext(object terminal)
+            {
+                _currentTerminal = terminal;
+            }
+
+            public Func<TContext, TProperty> GetContinuation()
+            {
+                return _continuation;
+            }
+
+            private TProperty ExecuteNext(TContext context)
+            {
+                return _chain.ExecuteAtIndex(_nextIndex, context, _currentTerminal);
+            }
         }
     }
 }
