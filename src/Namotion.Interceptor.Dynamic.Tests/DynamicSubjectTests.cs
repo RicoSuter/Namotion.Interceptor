f8b0f8b4b8afc8ae1166f3af6ba3937ae5e3015c
Rico Suter <mail@rsuter.com>
2025-08-29 16:05:44 +0200

    missing files

---
+++ src/Namotion.Interceptor.Dynamic.Tests/DynamicSubjectTests.cs
@@ -0,0 +1,96 @@
+﻿namespace Namotion.Interceptor.Dynamic.Tests;
+
+public interface IMotor
+{
+    int Speed { get; set; }
+}
+
+public interface ISensor
+{
+    int Temperature { get; set; }
+}
+
+public class DynamicSubjectTests
+{
+    [Fact]
+    public void WhenCreatingDynamicSubject_ThenItImplementsInterfaces()
+    {
+        // Act
+        var subject = DynamicSubject.Create(null, typeof(IMotor), typeof(ISensor));
+        var motor = (IMotor)subject;
+        var sensor = (ISensor)subject;
+
+        // Assert
+        motor.Speed = 100;
+        sensor.Temperature = 25;
+
+        Assert.Equal(100, motor.Speed);
+        Assert.Equal(25, sensor.Temperature);
+    }
+    
+    [Fact]
+    public Task WhenInterceptingDynamicSubject_ThenTheyAreCalled()
+    {
+        // Act
+        var logs = new List<string>();
+        
+        var context = InterceptorSubjectContext
+            .Create()
+            .WithService(() => new TestInterceptor("a", logs), _ => false)
+            .WithService(() => new TestInterceptor("b", logs), _ => false);
+
+        var subject = DynamicSubject.Create(context, typeof(IMotor), typeof(ISensor));
+        var motor = (IMotor)subject;
+        var sensor = (ISensor)subject;
+
+        // Act
+        motor.Speed = 100;
+        sensor.Temperature = 25;
+        ((IInterceptorSubject)subject).Context.RemoveFallbackContext(context);
+
+        // Assert & Act (read)
+        Assert.Equal(102, motor.Speed);
+        Assert.Equal(27, sensor.Temperature);
+        
+        // Assert
+        return Verify(logs);
+    }
+
+    public class TestInterceptor : IReadInterceptor, IWriteInterceptor, ILifecycleInterceptor
+    {
+        private readonly string _name;
+        private readonly List<string> _logs;
+
+        public TestInterceptor(string name, List<string> logs)
+        {
+            _name = name;
+            _logs = logs;
+        }
+
+        public TProperty ReadProperty<TProperty>(ref ReadPropertyInterception context, ReadInterceptionFunc<TProperty> next)
+        {
+            _logs.Add($"{_name}: Before read {context.Property.Name}");
+            var result = next(ref context);
+            _logs.Add($"{_name}: After read {context.Property.Name}");
+            return result;
+        }
+
+        public void WriteProperty<TProperty>(ref WritePropertyInterception<TProperty> context, WriteInterceptionAction<TProperty> next)
+        {
+            _logs.Add($"{_name}: Before write {context.Property.Name}");
+            context.NewValue = (TProperty)(object)((int)((object)context.NewValue!) + 1);
+            next(ref context);
+            _logs.Add($"{_name}: After write {context.Property.Name}");
+        }
+
+        public void AttachTo(IInterceptorSubject subject)
+        {
+            _logs.Add($"{_name}: Attached");
+        }
+
+        public void DetachFrom(IInterceptorSubject subject)
+        {
+            _logs.Add($"{_name}: Detached");
+        }
+    }
+}
