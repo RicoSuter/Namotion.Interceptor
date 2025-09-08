7eda16b869922ecc5fdab35ce240c7927b33e17f
Rico Suter <mail@rsuter.com>
2025-09-09 00:53:15 +0200

    avoid allocs

---
+++ src/Namotion.Interceptor.Tracking/PropertyReferenceDataExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Namotion.Interceptor.Tracking;
+
+public static class PropertyReferenceDataExtensions
+{
+    public static T GetOrAddPropertyData<T>(this PropertyReference property, string key, Func<T> valueFactory)
+    {
+        return (T)property.Subject.Data.GetOrAdd($"{property.Name}:{key}", static (_, f) => f(), valueFactory)!;
+    }
+}
