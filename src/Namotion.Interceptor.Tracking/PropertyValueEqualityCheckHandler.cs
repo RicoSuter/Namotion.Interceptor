6ffdf6103b3960ddb7a72da0a6175258dda3dc05
Rico Suter <mail@rsuter.com>
2025-09-09 10:57:38 +0200

    revert equals

--- src/Namotion.Interceptor.Tracking/PropertyValueEqualityCheckHandler.cs
+++ src/Namotion.Interceptor.Tracking/PropertyValueEqualityCheckHandler.cs
@@ -4,14 +4,7 @@ public class PropertyValueEqualityCheckHandler : IWriteInterceptor
 {
     public void WriteProperty<TProperty>(ref WritePropertyInterception<TProperty> context, WriteInterceptionAction<TProperty> next)
     {
-        if (typeof(TProperty).IsValueType || typeof(TProperty) == typeof(string))
-        {
-            if (!EqualityComparer<TProperty>.Default.Equals(context.CurrentValue, context.NewValue))
-            {
-                next(ref context);
-            }
-        }
-        else
+        if (!EqualityComparer<TProperty>.Default.Equals(context.CurrentValue, context.NewValue))
         {
             next(ref context);
         }
