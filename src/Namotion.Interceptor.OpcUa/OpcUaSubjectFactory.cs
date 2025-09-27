1419f8f0c06b03f06718a9e9766c0f107572742c
Rico Suter <mail@rsuter.com>
2025-09-27 17:08:38 +0100

    add

---
+++ src/Namotion.Interceptor.OpcUa/OpcUaSubjectFactory.cs
@@ -0,0 +1,23 @@
+﻿using Namotion.Interceptor.Registry.Abstractions;
+using Namotion.Interceptor.Sources;
+using Opc.Ua;
+using Opc.Ua.Client;
+
+namespace Namotion.Interceptor.OpcUa;
+
+public class OpcUaSubjectFactory
+{
+    private readonly ISubjectFactory _subjectFactory;
+
+    public OpcUaSubjectFactory(ISubjectFactory subjectFactory)
+    {
+        _subjectFactory = subjectFactory;
+    }
+
+    public Task<IInterceptorSubject> CreateSubjectAsync(RegisteredSubjectProperty property, ReferenceDescription node, Session session, CancellationToken cancellationToken)
+    {
+        var newSubject = _subjectFactory.CreateSubject(property);
+        // TODO: Look up HasTypeDefinition, resolve type and create dynamic subject if mapping available
+        return Task.FromResult(newSubject);
+    }
+}
