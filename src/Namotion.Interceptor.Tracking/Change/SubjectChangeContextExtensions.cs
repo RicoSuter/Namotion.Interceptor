d07358be840d11d8ab5185ea5bb56dd10794d6ca
Rico Suter <mail@rsuter.com>
2025-10-25 23:28:57 +0200

    up

---
+++ src/Namotion.Interceptor.Tracking/Change/SubjectChangeContextExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Namotion.Interceptor.Tracking.Change;
+
+public static class SubjectChangeContextExtensions
+{
+    /// <summary>
+    /// Sets the value of the property from the given source, changed and received timestamp.
+    /// </summary>
+    /// <param name="property">The property to mutate.</param>
+    /// <param name="source">The source.</param>
+    /// <param name="changedTimestamp">The changed timestamp.</param>
+    /// <param name="receivedTimestamp">The received timestamp.</param>
+    /// <param name="valueFromSource">The value.</param>
+    public static void SetValueFromSource(
+        this PropertyReference property, 
+        object source, DateTimeOffset? changedTimestamp, DateTimeOffset? receivedTimestamp, 
+        object? valueFromSource)
+    {
+        using (SubjectChangeContext.Apply(source, changedTimestamp, receivedTimestamp))
+        {
+            property.Metadata.SetValue?.Invoke(property.Subject, valueFromSource);
+        }
+    }
+}
