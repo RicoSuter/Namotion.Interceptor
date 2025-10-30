d54244b2adf2334723c7732e44e591637287e984
Rico Suter <mail@rsuter.com>
2025-10-31 00:56:36 +0100

    refactor

--- src/Namotion.Interceptor.Tracking/Change/PropertyChangedChannel.cs
+++ src/Namotion.Interceptor.Tracking/Change/PropertyChangedChannel.cs
@@ -6,38 +6,41 @@ namespace Namotion.Interceptor.Tracking.Change;
 public sealed class PropertyChangedChannel : IWriteInterceptor, IDisposable
 {
     private volatile PropertyChangedChannelSubscription[] _subscriptions = [];
-    private readonly Lock _gate = new();
+    private readonly Lock _subscriptionsModificationLock = new();
 
     public PropertyChangedChannelSubscription Subscribe()
     {
         var subscription = new PropertyChangedChannelSubscription(this);
-        lock (_gate)
+        lock (_subscriptionsModificationLock)
         {
-            var newArray = new PropertyChangedChannelSubscription[_subscriptions.Length + 1];
-            Array.Copy(_subscriptions, newArray, _subscriptions.Length);
-            newArray[_subscriptions.Length] = subscription;
-            _subscriptions = newArray;
+            var subscriptions = _subscriptions; // volatile read
+            var updatedSubscriptions = new PropertyChangedChannelSubscription[subscriptions.Length + 1];
+            Array.Copy(subscriptions, updatedSubscriptions, subscriptions.Length);
+            updatedSubscriptions[subscriptions.Length] = subscription;
+            _subscriptions = updatedSubscriptions;
         }
         return subscription;
     }
 
     public void Unsubscribe(PropertyChangedChannelSubscription subscription)
     {
-        lock (_gate)
+        lock (_subscriptionsModificationLock)
         {
-            var index = Array.IndexOf(_subscriptions, subscription);
+            var subscriptions = _subscriptions; // volatile read
+            var index = Array.IndexOf(subscriptions, subscription);
             if (index >= 0)
             {
-                var newArray = new PropertyChangedChannelSubscription[_subscriptions.Length - 1];
-                Array.Copy(_subscriptions, 0, newArray, 0, index);
-                Array.Copy(_subscriptions, index + 1, newArray, index, _subscriptions.Length - index - 1);
-                _subscriptions = newArray;
+                var updatedSubscriptions = new PropertyChangedChannelSubscription[subscriptions.Length - 1];
+                Array.Copy(subscriptions, 0, updatedSubscriptions, 0, index);
+                Array.Copy(subscriptions, index + 1, updatedSubscriptions, index, _subscriptions.Length - index - 1);
+                _subscriptions = updatedSubscriptions;
             }
         }
     }
 
     public void WriteProperty<TProperty>(ref PropertyWriteContext<TProperty> context, WriteInterceptionDelegate<TProperty> next)
     {
+        // ReSharper disable once InconsistentlySynchronizedField
         var subscriptions = _subscriptions; // volatile read
         if (subscriptions.Length == 0)
         {
@@ -64,6 +67,7 @@ public sealed class PropertyChangedChannel : IWriteInterceptor, IDisposable
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private void Enqueue(SubjectPropertyChange change)
     {
+        // ReSharper disable once InconsistentlySynchronizedField
         var subscriptions = _subscriptions; // volatile read
         for (int i = 0; i < subscriptions.Length; i++)
         {
@@ -73,6 +77,7 @@ public sealed class PropertyChangedChannel : IWriteInterceptor, IDisposable
 
     public void Dispose()
     {
+        // ReSharper disable once InconsistentlySynchronizedField
         var subscriptions = _subscriptions; // volatile read
         for (int i = 0; i < subscriptions.Length; i++)
         {
