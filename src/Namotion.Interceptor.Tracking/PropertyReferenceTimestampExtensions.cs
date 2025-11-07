9480d8c3de2eb3b34c44231cca20898b79d6d3d8
Rico Suter <mail@rsuter.com>
2025-11-08 00:56:05 +0100

    fix: Set timestamp of derived property on attach (#96)

--- src/Namotion.Interceptor.Tracking/PropertyReferenceTimestampExtensions.cs
+++ src/Namotion.Interceptor.Tracking/PropertyReferenceTimestampExtensions.cs
@@ -2,7 +2,7 @@ namespace Namotion.Interceptor.Tracking;
 
 public static class PropertyReferenceTimestampExtensions
 {
-    private const string ChangedTimestampKey = "Namotion.Interceptor.ChangedTimestamp";
+    private const string WriteTimestampKey = "Namotion.Interceptor.WriteTimestamp";
 
     /// <summary>
     /// Gets the timestamp of the last write operation (requires <see cref="InterceptorSubjectContextExtensions.WithLifecycle"/> or <see cref="InterceptorSubjectContextExtensions.WithDerivedPropertyChangeDetection"/>).
@@ -11,13 +11,13 @@ public static class PropertyReferenceTimestampExtensions
     /// <returns>The timestamp.</returns>
     public static DateTimeOffset? TryGetWriteTimestamp(this PropertyReference property)
     {
-        return property.GetOrAddPropertyData(ChangedTimestampKey, () => new DateTimeOffsetWrapper()).Value;
+        return property.GetOrAddPropertyData(WriteTimestampKey, () => new DateTimeOffsetWrapper()).Value;
     }
 
     internal static void SetWriteTimestamp(this PropertyReference property, DateTimeOffset timestamp)
     {
         property.AddOrUpdatePropertyData<DateTimeOffsetWrapper, DateTimeOffset>(
-            ChangedTimestampKey, static (wrapper, ts) => wrapper.Value = ts, timestamp);
+            WriteTimestampKey, static (wrapper, ts) => wrapper.Value = ts, timestamp);
     }
     
     // Wrapper used to avoid boxing of DateTimeOffset and ensure allocation free timestamp tracking
