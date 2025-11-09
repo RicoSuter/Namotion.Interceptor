ea7e4451b4855f1c8ded7d3a3c33f53469e54653
Rico Suter <mail@rsuter.com>
2025-11-09 23:34:40 +0100

    up

--- src/Namotion.Interceptor.OpcUa/Server/OpcUaSubjectServer.cs
+++ src/Namotion.Interceptor.OpcUa/Server/OpcUaSubjectServer.cs
@@ -1,11 +1,28 @@
-﻿using Opc.Ua.Server;
+﻿using Microsoft.Extensions.Logging;
+using Opc.Ua.Server;
 
 namespace Namotion.Interceptor.OpcUa.Server;
 
 internal class OpcUaSubjectServer : StandardServer
 {
-    public OpcUaSubjectServer(IInterceptorSubject subject, OpcUaSubjectServerSource source, OpcUaServerConfiguration configuration)
+    private readonly ILogger _logger;
+
+    public OpcUaSubjectServer(IInterceptorSubject subject, OpcUaSubjectServerSource source, OpcUaServerConfiguration configuration, ILogger logger)
     {
+        _logger = logger;
         AddNodeManager(new CustomNodeManagerFactory(subject, source, configuration));
+    }
+
+    protected override void OnServerStarted(IServerInternal server)
+    {
+        server.SessionManager.SessionCreated += (s, _) =>
+        {
+            _logger.LogInformation("OPC UA session {SessionId} created.", s.Id);
+        };
+
+        server.SessionManager.SessionClosing += (s, _) =>
+        {
+            _logger.LogInformation("OPC UA session {SessionId} closing.", s.Id);
+        };
     }
 }
