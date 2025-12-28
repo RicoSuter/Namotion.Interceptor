6fe7c3e565996381b0742306d12d2bae8d612e4d
Rico Suter <mail@rsuter.com>
2025-12-28 16:20:19 +0100

    refactor

--- src/Namotion.Interceptor.Tracking/Transactions/SubjectPropertyChangeExtensions.cs
+++ src/Namotion.Interceptor.Tracking/Transactions/SubjectPropertyChangeExtensions.cs
@@ -8,28 +8,19 @@ namespace Namotion.Interceptor.Tracking.Transactions;
 internal static class SubjectPropertyChangeExtensions
 {
     /// <summary>
-    /// Creates a rollback change by swapping old and new values.
-    /// Used to revert a previously applied change.
-    /// </summary>
-    /// <param name="change">The original change to create a rollback for.</param>
-    /// <returns>A new change that will restore the original value.</returns>
-    public static SubjectPropertyChange ToRollbackChange(this SubjectPropertyChange change) =>
-        SubjectPropertyChange.Create(
-            change.Property,
-            source: change.Source,
-            changedTimestamp: change.ChangedTimestamp,
-            receivedTimestamp: change.ReceivedTimestamp,
-            oldValue: change.GetNewValue<object?>(),
-            newValue: change.GetOldValue<object?>());
-
-    /// <summary>
     /// Creates rollback changes for a collection, reversing order for proper undo sequence.
     /// </summary>
     /// <param name="changes">The changes to create rollbacks for.</param>
     /// <returns>Rollback changes in reverse order.</returns>
     public static IEnumerable<SubjectPropertyChange> ToRollbackChanges(
         this IEnumerable<SubjectPropertyChange> changes) =>
-        changes.Reverse().Select(c => c.ToRollbackChange());
+        changes.Reverse().Select(c => SubjectPropertyChange.Create(
+            c.Property,
+            source: c.Source,
+            changedTimestamp: c.ChangedTimestamp,
+            receivedTimestamp: c.ReceivedTimestamp,
+            oldValue: c.GetNewValue<object?>(),
+            newValue: c.GetOldValue<object?>()));
 
     /// <summary>
     /// Applies a single property change to the in-process model.
@@ -37,7 +28,7 @@ internal static class SubjectPropertyChangeExtensions
     /// <param name="change">The change to apply.</param>
     /// <param name="error">The exception if the change failed, null otherwise.</param>
     /// <returns>True if successful, false if an exception occurred.</returns>
-    public static bool TryApply(this SubjectPropertyChange change, out Exception? error)
+    public static bool TryApplyChange(this SubjectPropertyChange change, out Exception? error)
     {
         try
         {
@@ -59,7 +50,7 @@ internal static class SubjectPropertyChangeExtensions
     /// <param name="changes">The changes to apply.</param>
     /// <returns>Lists of successful changes, failed changes, and errors.</returns>
     public static (List<SubjectPropertyChange> Successful, List<SubjectPropertyChange> Failed, List<Exception> Errors)
-        ApplyAll(this IEnumerable<SubjectPropertyChange> changes)
+        ApplyAllChanges(this IEnumerable<SubjectPropertyChange> changes)
     {
         var successful = new List<SubjectPropertyChange>();
         var failed = new List<SubjectPropertyChange>();
@@ -67,7 +58,7 @@ internal static class SubjectPropertyChangeExtensions
 
         foreach (var change in changes)
         {
-            if (change.TryApply(out var error))
+            if (change.TryApplyChange(out var error))
             {
                 successful.Add(change);
             }
