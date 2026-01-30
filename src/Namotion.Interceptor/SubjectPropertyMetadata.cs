4c8dfed36d73f0e0a1c62079e9754c1a70dad257
Rico Suter <mail@rsuter.com>
2026-01-30 16:19:55 +0100

    feature: Add interface property attribute inheritance

--- src/Namotion.Interceptor/SubjectPropertyMetadata.cs
+++ src/Namotion.Interceptor/SubjectPropertyMetadata.cs
@@ -66,7 +66,7 @@ public readonly record struct SubjectPropertyMetadata
         : this(
             propertyInfo.Name,
             propertyInfo.PropertyType,
-            propertyInfo.GetCustomAttributes().ToArray(),
+            propertyInfo.GetCustomAttributesWithInterfaceInheritance(),
             getValue,
             setValue,
             isIntercepted,
