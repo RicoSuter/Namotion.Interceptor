f31e0b189dcb8523da6c046529a0ac5ce00043ad
Rico Suter <mail@rsuter.com>
2026-04-07 00:18:39 +0200

    feat: add WallboxChargingSession class and enhance WallboxCharger state management

--- src/HomeBlaze/Namotion.Devices.Wallbox/Model/AuthResponse.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace Namotion.Devices.Wallbox.Model;
-
-internal class AuthResponse
-{
-    [JsonPropertyName("jwt")]
-    public string? Jwt { get; set; }
-}
