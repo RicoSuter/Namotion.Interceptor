b8a4b2dd2fda806d0b8333e129be73f20400a95a
Rico Suter <mail@rsuter.com>
2026-05-10 02:02:58 +0200

    build(connector-tester): add ConnectorTester.Tests xUnit project

---
+++ src/Namotion.Interceptor.ConnectorTester.Tests/Engine/SnapshotComparerTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Namotion.Interceptor.ConnectorTester.Tests.Engine;
+
+public class SnapshotComparerTests
+{
+    [Fact]
+    public void WhenProjectIsCreated_ThenPlaceholderTestPasses()
+    {
+        // Arrange / Act / Assert
+        Assert.True(true);
+    }
+}
